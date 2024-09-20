d642a41ad2caf3e6fd47e6ef66ae1f83ddc3f810
Thiago <thiago_ssfr@hotmail.com>
2024-09-20 18:54:46 -0300

    teste

--- src/main.py
+++ src/main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Numbers(BaseModel):
+    num1: float
+    num2: float
+
+
+class Number(BaseModel):
+    num: int
+
+
+@app.post("/sum")
+async def odd_even(number: Number):
+    result = number.num % 2
+    return {"odd_even": "odd" if result != 0 else "even"}
+
+
+@app.post("/sum")
+async def sum(numbers: Numbers):
+    result = numbers.num1 + numbers.num2
+    return {"sum": result}
 
 
 @app.get("/ping")
--- test/test_main.py
+++ test/test_main.py
@@ -1,8 +1,44 @@
-from src.main import root
-from unittest.mock import patch
+from src.main import root, ping, sum, odd_even
+from src.main import Numbers, Number
 import pytest
+
 
 @pytest.mark.asyncio
 async def test_root():
     result = await root()
     assert result == {"message": "Hello, World!"}
+
+
+@pytest.mark.asyncio
+async def test_ping():
+    result = await ping()
+    assert result == {"message": "pong"}
+
+
+@pytest.mark.asyncio
+async def test_sum_error_no_body():
+    with pytest.raises(TypeError) as excinfo:
+        await sum()
+
+    assert str(excinfo.value) == "sum() missing 1 required positional argument: 'numbers'"
+
+
+@pytest.mark.asyncio
+async def test_sum_success():
+    numbers = Numbers(num1=10, num2=5)
+    result = await sum(numbers)
+    assert result == {'sum': 15.0}
+
+
+@pytest.mark.asyncio
+async def test_even():
+    number = Number(num=10)
+    result = await odd_even(number)
+    assert result == {"odd_even": "even"}
+
+
+@pytest.mark.asyncio
+async def test_odd():
+    number = Number(num=5)
+    result = await odd_even(number)
+    assert result == {"odd_even": "odd"}
